61a36c2d4d2358c92d542d720e5fd1e1e5542af3
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-09-03 15:23:12 +0200

    DAF - v1.0

---
+++ daf.py
@@ -0,0 +1,39 @@
+from data import dataToFix
+import os
+
+def getFiles(path:str, fileType:str):
+    '''Returns a list of the filenames with the specified extension'''
+    filesList = []
+    for path, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(fileType):
+                filesList.append(f"{path}/{file}")
+    filesList.sort()
+    return filesList
+
+def getFilesFromDict(dataDict:dict, files:list):
+    '''Returns a list of files which contains the specified data'''
+    filesList = []
+    for file in files:
+        with open(file, 'r') as curFile:
+            for key in dataDict.keys():
+                for line in curFile.readlines():
+                    if line.find(key) != -1:
+                        filesList.append(curFile.name)
+    filesList.sort()
+    return filesList
+
+def replaceOldData(path:str, extension:str):
+    '''Returns list of the new files to write'''
+    for data in dataToFix:
+        files = getFilesFromDict(data, getFiles(path, extension))
+        for file in files:
+            with open(file, 'r') as curFile:
+                fileData = curFile.read()
+            for key in data.keys():
+                fileData = fileData.replace(f"{key} ", f"{data[key]} ")
+            with open(file, 'w') as curFile:
+                curFile.write(fileData)
+
+replaceOldData("../oot/assets", ".h")
+replaceOldData("../oot/assets", ".c")
---
+++ data.py
@@ -0,0 +1,34 @@
+camData = {
+    "CamData": "BgCamInfo",
+}
+
+curveData = {
+    "TransformData": "CurveInterpKnot",
+    "TransformUpdateIndex": "CurveAnimationHeader",
+    "SkelCurveLimbList": "CurveSkeletonHeader",
+}
+
+roomData = {
+    "PolygonDlist": "RoomShapeDListsEntry",
+    "PolygonType0": "RoomShapeNormal",
+    "MeshHeader1Single": "RoomShapeImageSingle",
+    "BgImage": "RoomShapeImageMultiBgEntry",
+    "MeshHeader1Multi": "RoomShapeImageMulti",
+    "PolygonDlist2": "RoomShapeCullableEntry",
+    "PolygonType2": "RoomShapeCullable",
+    "SCENE_CMD_MESH": "SCENE_CMD_ROOM_SHAPE",
+}
+
+skinData = {
+    "Struct_800A57C0": "SkinVertex",
+    "Struct_800A598C_2": "SkinTransformation",
+    "Struct_800A5E28": "SkinAnimatedLimbData",
+    "Struct_800A598C": "SkinLimbModif",
+}
+
+dataToFix = [
+    camData,
+    curveData,
+    roomData,
+    skinData,
+]
